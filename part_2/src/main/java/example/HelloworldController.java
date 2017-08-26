2d3aaddeabc2bfd7b5ffa590a5b75f7bb6f20d0c
Krystian Kałużny <k.kaluzny141@gmail.com>
2017-08-26 18:42:31 +0200

    Java FX + AOP Workaround

--- part_2/src/main/java/example/HelloworldController.java
+++ part_2/src/main/java/example/HelloworldController.java
@@ -16,14 +16,45 @@ public class HelloworldController {
   
     @FXML
     private TextField nameField;
-    
-    // Be aware: This is a Spring bean. So we can do the following:
+
     @Autowired
     private AwesomeActionService actionService;
-    
+
+    protected void init()
+    {
+        // this class is the HelloworldController class because init() is not private method
+        // do init staff if you want
+        // now FML fields are not null
+    }
+
     @FXML
-    private void setHelloText(final Event event) {
+    public void setHelloText(final Event event) {
+        // this method has to be not private
+        // now FML fields are not null
        final String textToBeShown = actionService.processName(nameField.getText());
        helloLabel.setText(textToBeShown); 
+    }
+
+    @FXML
+    private void initialize()
+    {
+        // this class is the HelloworldController$$EnhancerBySpringCGLB$$BlaBla class because initialize() is private method
+        // so fxml fields are not null and autowired fields are null
+        // pass this.helloLabel and this.nameField to delegated HelloworldController object
+        setHelloLabel(helloLabel);
+        setNameField(nameField);
+        init();
+    }
+
+    protected void setHelloLabel(Label helloLabel) {
+        // this class is the HelloworldController class because setHelloLabel() is not private method
+        // helloLabel argument comes from HelloworldController$$EnhancerBySpringCGLB$$BlaBla
+        this.helloLabel = helloLabel;
+    }
+
+    protected void setNameField(TextField nameField) {
+        // this class is the HelloworldController class because setNameField() is not private method
+        // nameField argument comes from HelloworldController$$EnhancerBySpringCGLB$$BlaBla
+        this.nameField = nameField;
     }
 }
